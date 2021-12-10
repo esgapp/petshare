82afdb770c14aa6501456a66e7345a04db3269e8
khknopp <kajetan@knopp.com.pl>
2021-12-10 19:56:06 +0100

    Added better main.py

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ from datetime import datetime
 from flask import Flask, flash, render_template, redirect, request, url_for, jsonify, session, Response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
-import levels
 
 # -----------^IMPORTS^---------------
 
 app = Flask(__name__)
-app.secret_key = 'ljrgregjorrejrekirew9843j'
+app.secret_key = 'petsharepetsharepetsharepetsharepetsharepetsharepetshare'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 
@@ -23,108 +22,31 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
-class Level(db.Model):
+class Item(db.Model):
     Id = db.Column(db.Integer, primary_key=True)
-    Qubits = db.Column(db.Integer, nullable=False)
-    Monsters = db.Column(db.Integer, nullable=False)
-    Gates = db.Column(db.String(50))
-    Description = db.Column(db.String(100), nullable=False)
-    Success = db.Column(db.String(50), nullable=False)
-    Failure = db.Column(db.String(50), nullable=False)
+    Name = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
-        return f"Level('{self.Id}', '{self.Qubits}', '{self.Monsters}', '{self.Gates}', '{self.Description}', '{self.Success}','{self.Failure}')"
+        return f"Item('{self.Id}', '{self.Name}')"
+
+class User(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    Username = db.Column(db.String(20), nullable=False)
+    Password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.Id}', '{self.Name}')"
 
 db.create_all()
 
 # -----------------^DATABASE^-----------------------
 
-# Defining the levels:
-if(Level.query.all() == []):
-    levels.createAllLevels(db, Level)
 
-
-# ------------------^FUNCTIONS^------------------------------
-
-@app.before_first_request
-def initialize():
-    session['level'] = 0
-
-@app.route('/logout')
-def logout():
-    session['level'] = 0
-    return redirect(url_for('introduction'))
+##### Homepage #####
 
 @app.route('/')
 def main():
-    if(session['level'] == 0):
-        return redirect(url_for('introduction'))
-    else:
-        return redirect(url_for('description'))
-
-###### Introduction ######
-@app.route('/introduction')
-def introduction():
-    return render_template('introduction.html')
-
-@app.route('/introduction_next')
-def introduction_text():
-    session['level'] = 1
-    return redirect(url_for('description'))
-
-
-###### Description ######
-@app.route('/description')
-def description():
-    if(session['level'] == 0 or session['level'] == None):
-        return redirect(url_for('introduction'))
-    description = Level.query.filter_by(Id=session['level']).first().Description
-    return render_template('description.html', level=session['level'], description=description)
-
-@app.route('/description_next')
-def description_next():
-    if(session['level'] == 0 or session['level'] == None):
-        return redirect(url_for('introduction'))
-    return redirect(url_for('level'))
-
-
-###### Level ######
-@app.route('/level')
-def level():
-    return render_template('level.html', level=session['level'])
-
-@app.route('/level_next')
-def level_next():
-    return redirect(url_for('success'))
-
-###### Level success ######
-@app.route('/success')
-def success():
-    success = Level.query.filter_by(Id=session['level']).first().Success
-    session['level'] += 1
-    return render_template('success.html', success=success, level=session['level']-1)
-
-@app.route('/success_next')
-def success_next():
-    if(session['level'] > db.session.query(func.max(Level.Id)).scalar()):
-        return redirect(url_for('endgame'))
-    return redirect(url_for('description'))
-
-###### Level failure ######
-@app.route('/failure')
-def failure():
-    failure = Level.query.filter_by(Id=session['level']).first().Failure
-    return render_template('failure.html', failure=failure, level=session['level'])
-
-@app.route('/failure_next')
-def failure_next():
-    return redirect(url_for('description'))
-
-###### End of game ######
-@app.route('/endgame')
-def endgame():
-    session['level'] = 0
-    return render_template('endgame.html')
+    return render_template('index.html')
 
 
 # -------^ROUTES^-------
